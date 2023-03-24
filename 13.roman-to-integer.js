fd9114e2e2517561f4c4c0be9ea4ecf834c48868
“Omar <ocamacho812@gmail.com>
2023-03-23 17:54:35 -0700

    Another problem

---
+++ 13.roman-to-integer.js
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode id=13 lang=javascript
+ *
+ * [13] Roman to Integer
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const romanToInt = (s) => {
+  if (s.length === 0) {
+    return 0;
+  }
+
+  const map = new Map([
+    ['I', 1],
+    ['V', 5],
+    ['X', 10],
+    ['L', 50],
+    ['C', 100],
+    ['D', 500],
+    ['M', 1000],
+  ]);
+
+  let i = s.length - 1;
+  let result = map.get(s[i]);
+
+  while (i > 0) {
+    const curr = map.get(s[i]);
+    const prev = map.get(s[i - 1]);
+
+    prev >= curr ? (result += prev) : (result -= prev);
+
+    i--;
+  }
+
+  return result;
+};
+// @lc code=end
