f278977957c8e0b08e47faa3b65b8c3635e42ed5
“Omar <ocamacho812@gmail.com>
2023-05-09 12:56:36 -0700

    Complete 125

---
+++ 125.valid-palindrome.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=125 lang=javascript
+ *
+ * [125] Valid Palindrome
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function (s) {
+  if (!s) return '';
+  
+  const longStr = s.replace(/[^a-z0-9]/gi, '').toLowerCase()
+
+  if (longStr === longStr.split('').reverse().join('')) {
+    return true
+  } else {
+    return false
+  }
+};
+// @lc code=end
