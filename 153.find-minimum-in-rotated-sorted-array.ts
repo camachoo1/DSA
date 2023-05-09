bee9d9f2e6ebf4a89609e92487917332c4f69c8a
“Omar <ocamacho812@gmail.com>
2023-05-09 14:17:21 -0700

    Complete 153

---
+++ 153.find-minimum-in-rotated-sorted-array.ts
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=153 lang=typescript
+ *
+ * [153] Find Minimum in Rotated Sorted Array
+ */
+
+// @lc code=start
+function findMin(nums: number[]): number {
+  let start = 0
+  let end = nums.length - 1
+
+  if (nums[start] < nums[end]) { 
+    return nums[start]
+  }
+
+  while (start < end) {
+    let mid = Math.floor((start + end) / 2)
+
+    if (nums[mid] > nums[end]) {
+      start = mid + 1
+    } else {
+      end = mid
+    }
+  }
+
+  return nums[start]
+};
+// @lc code=end
+
