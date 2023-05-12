3c4e2303d74ca654bae1b6b8c3bb3952ba4e8a25
“Omar <ocamacho812@gmail.com>
2023-05-12 13:13:47 -0700

    Add 240

---
+++ 240.search-a-2-d-matrix-ii.ts
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=240 lang=typescript
+ *
+ * [240] Search a 2D Matrix II
+ */
+
+// @lc code=start
+function searchMatrix(matrix: number[][], target: number): boolean {
+  let row: number = 0
+  let col: number = matrix[row].length - 1
+
+  while (row < matrix.length && col >= 0) {
+    if (matrix[row][col] === target) {
+      return true
+    }
+
+    if (matrix[row][col] < target) {
+      row++
+    } else {
+      col--
+    }
+  }
+  return false
+};
+// @lc code=end
+
