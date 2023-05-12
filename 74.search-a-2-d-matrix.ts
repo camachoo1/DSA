37d59b285eecd072135447b6c0122b4b9febdfdc
“Omar <ocamacho812@gmail.com>
2023-05-12 15:03:32 -0700

    Add 74

--- 74.search-a-2-d-matrix.ts
+++ 74.search-a-2-d-matrix.ts
@@ -6,19 +6,34 @@
 
 // @lc code=start
 function searchMatrix(matrix: number[][], target: number): boolean {
-  let row = matrix.length - 1
-  let col = 0
+  let left = 0;
+  let right = matrix.length - 1;
 
-  while (row >= 0 && col < matrix[0].length) {
-    if (matrix[row][col] === target) {
-      return true
-    } else if (matrix[row][col] > target) {
-      row--
-    } else {
-      col++
-    }
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    const first = matrix[mid][0];
+    const last = matrix[mid][matrix[mid].length - 1];
+    if (target === first || target === last) return true;
+    if (target > first && target < last)
+      return binarySearch(matrix[mid], target);
+    if (target < first) right = mid - 1;
+    else left = mid + 1;
   }
-  return false
+  return false;
+}
+
+const binarySearch = (nums: number[], target: number): boolean => {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (nums[mid] === target) return true;
+    if (nums[mid] < target) left = mid + 1;
+    else right = mid - 1;
+  }
+
+  return false;
 };
 // @lc code=end
 
