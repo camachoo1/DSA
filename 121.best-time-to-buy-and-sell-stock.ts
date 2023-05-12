db927647ace4801aa502de8d311ebda55ee6d786
“Omar <ocamacho812@gmail.com>
2023-05-12 13:23:26 -0700

    Add 121

---
+++ 121.best-time-to-buy-and-sell-stock.ts
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=121 lang=typescript
+ *
+ * [121] Best Time to Buy and Sell Stock
+ */
+
+// @lc code=start
+function maxProfit(prices: number[]): number {
+  let minPrice = prices[0];
+  let maxProfit = 0
+
+  for (let i = 1; i < prices.length; i++) {
+    const profit = prices[i] - minPrice
+    if (profit > maxProfit) { 
+      maxProfit = profit
+    }
+
+    if (prices[i] < minPrice) {
+      minPrice = prices[i]
+    }
+  }
+  return maxProfit
+};
+// @lc code=end
+
