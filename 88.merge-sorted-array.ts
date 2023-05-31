965a7581f1ff0c92a703164d5ee8a1992e1ce4df
“Omar <ocamacho812@gmail.com>
2023-05-31 13:13:46 -0700

    Add 88

---
+++ 88.merge-sorted-array.ts
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=88 lang=typescript
+ *
+ * [88] Merge Sorted Array
+ */
+
+// @lc code=start
+/**
+ Do not return anything, modify nums1 in-place instead.
+ */
+function merge(nums1: number[], m: number, nums2: number[], n: number): void {
+  const index = nums1.length - n;
+
+  for (let i = index; i < nums1.length; i++) {
+    nums1[i] = nums2[i - index]
+  }
+
+  nums1.sort((a, b) => a - b)
+};
+
+// @lc code=end
+
