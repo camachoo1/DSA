cda5be9ea64da9cf1b35dc6bd71b5e0f01953b54
“Omar <ocamacho812@gmail.com>
2023-05-31 14:30:36 -0700

    Add 283

---
+++ 283.move-zeroes.ts
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=283 lang=typescript
+ *
+ * [283] Move Zeroes
+ */
+
+// @lc code=start
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function moveZeroes(nums: number[]): void {
+  let count = 0
+  for (const num of nums) {
+    if (num !== 0) {
+      nums[count++] = num
+    }
+  }
+
+  for (let i = count; i < nums.length; i++) {
+    nums[i] = 0
+  }
+};
+// @lc code=end
+
