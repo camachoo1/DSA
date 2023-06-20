73e61659df21c9161f72c1711580af504f9a2baf
“Omar <ocamacho812@gmail.com>
2023-06-19 21:39:51 -0700

    Add 58

---
+++ 58.length-of-last-word.ts
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=58 lang=typescript
+ *
+ * [58] Length of Last Word
+ */
+
+// @lc code=start
+function lengthOfLastWord(s: string): number {
+  let length = 0
+  let started = false
+
+  for (let i = s.length - 1; i >= 0; i--) {
+    if (s[i] !== ' ') {
+      length++
+      started = true
+    } else if (started) {
+      break
+    }
+  }
+
+  return length
+};
+// @lc code=end
+
