fbb59c9f700509698cedca1590247ae4931f7f27
“Omar <ocamacho812@gmail.com>
2023-06-19 22:42:17 -0700

    Add 153

--- 153.find-minimum-in-rotated-sorted-array.ts
+++ 153.find-minimum-in-rotated-sorted-array.ts
@@ -6,24 +6,24 @@
 
 // @lc code=start
 function findMin(nums: number[]): number {
-  let start = 0
-  let end = nums.length - 1
+  let start = 0;
+  let end = nums.length - 1;
 
   if (nums[start] < nums[end]) { 
     return nums[start]
-  }
+  };
 
   while (start < end) {
-    const mid = Math.floor((start + end) / 2)
+    const mid = Math.floor((start + end) / 2);
 
     if (nums[mid] > nums[end]) {
       start = mid + 1
     } else {
       end = mid
-    }
-  }
+    };
+  };
 
-  return nums[start]
+  return nums[start];
 };
 // @lc code=end
 
