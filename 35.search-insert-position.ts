e6cde03f0f8802142caf8bcb9df6a5751d3daaa1
“Omar <ocamacho812@gmail.com>
2023-08-08 15:37:23 -0700

    Add 28

---
+++ 35.search-insert-position.ts
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=35 lang=typescript
+ *
+ * [35] Search Insert Position
+ */
+
+// @lc code=start
+function searchInsert(nums: number[], target: number): number {
+
+  let start = 0;
+  let end = nums.length - 1;
+
+  while (start <= end) {
+    const mid = Math.floor((start + end) / 2);
+
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] > target) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+
+  return start
+};
+// @lc code=end
+
