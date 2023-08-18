46762d60858b6464c0c6eed2e2d2212b537c591f
“Omar <ocamacho812@gmail.com>
2023-08-18 00:06:54 -0700

    Add 118

---
+++ 118.pascals-triangle.ts
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=118 lang=typescript
+ *
+ * [118] Pascal's Triangle
+ */
+
+// @lc code=start
+function generate(numRows: number): number[][] {
+  const res: number[][] = []
+
+  for (let i = 0; i < numRows; i++) {
+    const row: number[] = []
+
+    for (let j = 0; j <= i; j++) {
+      if (j === 0 || j === i) {
+        row.push(1)
+      } else {
+        const previousRow = res[i - 1]
+        const newValue = previousRow[j - 1] + previousRow[j]
+        row.push(newValue)
+      }
+    }
+    res.push(row)
+    
+  }
+
+  return res
+};
+// @lc code=end
+
