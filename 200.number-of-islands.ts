74329820d8c8904928bb8404e41f0347d2b13515
“Omar <ocamacho812@gmail.com>
2023-08-31 00:04:32 -0700

    Add 200

--- 200.number-of-islands.ts
+++ 200.number-of-islands.ts
@@ -6,19 +6,19 @@
 
 // @lc code=start
 function numIslands(grid: string[][]): number {
-  let count: number = 0
+  let count: number = 0;
 
   for (let row = 0; row < grid.length; row++) {
     for (let col = 0; col < grid[row].length; col++) {
       if (grid[row][col] === '1') {
-        count++
-        dfs(row, col, grid)
+        count++;
+        dfs(row, col, grid);
       }
     }
   }
 
-  return count
-};
+  return count;
+}
 
 const dfs = (row: number, col: number, grid: string[][]): void => {
   if (
@@ -28,15 +28,14 @@ const dfs = (row: number, col: number, grid: string[][]): void => {
     col >= grid[row].length ||
     grid[row][col] === '0'
   ) {
-    return
+    return;
   }
 
-  grid[row][col] = '0'
+  grid[row][col] = '0';
 
-  dfs(row, col + 1, grid)
-  dfs(row, col - 1, grid)
-  dfs(row + 1, col, grid)
-  dfs(row - 1, col, grid)
-}
+  dfs(row, col + 1, grid);
+  dfs(row, col - 1, grid);
+  dfs(row + 1, col, grid);
+  dfs(row - 1, col, grid);
+};
 // @lc code=end
-
