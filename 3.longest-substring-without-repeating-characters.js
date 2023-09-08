02e4084015e2da5107b533b878043db6e725c929
“Omar <ocamacho812@gmail.com>
2023-09-07 23:55:01 -0700

    Add 3

--- 3.longest-substring-without-repeating-characters.js
+++ 3.longest-substring-without-repeating-characters.js
@@ -9,25 +9,24 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function(s) {
-  const set = new Set()
+var lengthOfLongestSubstring = function (s) {
+  const set = new Set();
   let start = 0;
-  let maxSize = 0
+  let maxSize = 0;
 
-  if (s.length === 0) return 0
-  if (s.length === 1) return 1
+  if (s.length === 0) return 0;
+  if (s.length === 1) return 1;
 
   for (let i = 0; i < s.length; i++) {
     while (set.has(s[i])) {
-      set.delete(s[start])
-      start++
+      set.delete(s[start]);
+      start++;
     }
 
-    set.add(s[i])
-    maxSize = Math.max(maxSize, i - start + 1)
+    set.add(s[i]);
+    maxSize = Math.max(maxSize, i - start + 1);
   }
 
-  return maxSize
+  return maxSize;
 };
 // @lc code=end
-
