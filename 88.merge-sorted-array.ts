74aebe13182644c8cd60844e23e3af78a6cbd501
“Omar <ocamacho812@gmail.com>
2023-09-07 23:59:08 -0700

    Add 88

--- 88.merge-sorted-array.ts
+++ 88.merge-sorted-array.ts
@@ -8,15 +8,19 @@
 /**
  Do not return anything, modify nums1 in-place instead.
  */
-function merge(nums1: number[], m: number, nums2: number[], n: number): void {
+function merge(
+  nums1: number[],
+  m: number,
+  nums2: number[],
+  n: number
+): void {
   const index = nums1.length - n;
 
   for (let i = index; i < nums1.length; i++) {
-    nums1[i] = nums2[i - index]
+    nums1[i] = nums2[i - index];
   }
 
-  nums1.sort((a, b) => a - b)
-};
+  nums1.sort((a, b) => a - b);
+}
 
 // @lc code=end
-
