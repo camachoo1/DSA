b9cab8a7835fef2c9f46d0f0c4eafe063a49bcad
“Omar <ocamacho812@gmail.com>
2023-09-07 23:56:45 -0700

    Add 34

--- 34.find-first-and-last-position-of-element-in-sorted-array.ts
+++ 34.find-first-and-last-position-of-element-in-sorted-array.ts
@@ -9,52 +9,49 @@ function searchRange(nums: number[], target: number): number[] {
   const firstElement = findFirstElement(nums, target);
   const lastElement = findLastElement(nums, target);
   return [firstElement, lastElement];
-};
+}
 
 function findFirstElement(nums: number[], target: number): number {
-  let left = 0
-  let right = nums.length - 1
-  let res = -1
+  let left = 0;
+  let right = nums.length - 1;
+  let res = -1;
 
   while (left <= right) {
-    const mid = Math.floor((left + right) / 2)
+    const mid = Math.floor((left + right) / 2);
 
-    
     if (nums[mid] >= target) {
-      right = mid - 1
+      right = mid - 1;
     } else {
-      left = mid + 1
+      left = mid + 1;
     }
 
     if (nums[mid] === target) {
-      res = mid
+      res = mid;
     }
   }
 
-  return res
+  return res;
 }
 
 function findLastElement(nums: number[], target: number): number {
-  let left = 0
-  let right = nums.length - 1
-  let res = -1
+  let left = 0;
+  let right = nums.length - 1;
+  let res = -1;
 
   while (left <= right) {
-    const mid = Math.floor((left + right) / 2)
+    const mid = Math.floor((left + right) / 2);
 
-    
     if (nums[mid] <= target) {
-      left = mid + 1
+      left = mid + 1;
     } else {
-      right = mid - 1
+      right = mid - 1;
     }
 
     if (nums[mid] === target) {
-      res = mid
+      res = mid;
     }
   }
 
-  return res
+  return res;
 }
 // @lc code=end
-
