61e72f47e7c47e5f18711e5f6e49223ea29d6d99
“Omar <ocamacho812@gmail.com>
2023-09-07 23:57:26 -0700

    Add 53

--- 53.maximum-subarray.ts
+++ 53.maximum-subarray.ts
@@ -6,17 +6,16 @@
 
 // @lc code=start
 function maxSubArray(nums: number[]): number {
-  if (nums.length === 0) return 0
+  if (nums.length === 0) return 0;
 
-  let maxEnd = nums[0]
-  let currentMax = nums[0]
+  let maxEnd = nums[0];
+  let currentMax = nums[0];
 
   for (let i = 1; i < nums.length; i++) {
-    maxEnd = Math.max(nums[i], maxEnd + nums[i])
-    currentMax = Math.max(maxEnd, currentMax)
+    maxEnd = Math.max(nums[i], maxEnd + nums[i]);
+    currentMax = Math.max(maxEnd, currentMax);
   }
 
-  return currentMax
-};
+  return currentMax;
+}
 // @lc code=end
-
