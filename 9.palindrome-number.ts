76b770db5a18591ae22244cf45f44b23659f492a
“Omar <ocamacho812@gmail.com>
2023-09-07 23:55:39 -0700

    Add 9

--- 9.palindrome-number.ts
+++ 9.palindrome-number.ts
@@ -6,19 +6,19 @@
 
 // @lc code=start
 function isPalindrome(x: number): boolean {
-  if (x < 0 || (x != 0 && x % 10 == 0)) return false
+  if (x < 0 || (x != 0 && x % 10 == 0)) return false;
 
-    let s: string = x.toString()
-    let l = 0, r = s.length - 1
+  let s: string = x.toString();
+  let l = 0,
+    r = s.length - 1;
 
-    while (l < r) {
-      if (s[l] !== s[r]) {
-        return false
-      }
-        l++, r--
+  while (l < r) {
+    if (s[l] !== s[r]) {
+      return false;
     }
+    l++, r--;
+  }
 
-    return true
-};
+  return true;
+}
 // @lc code=end
-
