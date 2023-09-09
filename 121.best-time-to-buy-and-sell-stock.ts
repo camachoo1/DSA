4fb2537a5ddc1be4810cb1be7e4f58af5c87f84c
“Omar <ocamacho812@gmail.com>
2023-09-08 23:54:51 -0700

    Add 121

--- 121.best-time-to-buy-and-sell-stock.ts
+++ 121.best-time-to-buy-and-sell-stock.ts
@@ -7,19 +7,18 @@
 // @lc code=start
 function maxProfit(prices: number[]): number {
   let minPrice = prices[0];
-  let maxProfit = 0
+  let maxProfit = 0;
 
   for (let i = 1; i < prices.length; i++) {
-    const profit = prices[i] - minPrice
-    if (profit > maxProfit) { 
-      maxProfit = profit
+    const profit = prices[i] - minPrice;
+    if (profit > maxProfit) {
+      maxProfit = profit;
     }
 
     if (prices[i] < minPrice) {
-      minPrice = prices[i]
+      minPrice = prices[i];
     }
   }
-  return maxProfit
-};
+  return maxProfit;
+}
 // @lc code=end
-
