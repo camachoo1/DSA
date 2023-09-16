af1c547d1b8bebc0e00452d33d471be7c03169cd
“Omar <ocamacho812@gmail.com>
2023-09-15 23:51:54 -0700

    Add 389

--- 389.find-the-difference.ts
+++ 389.find-the-difference.ts
@@ -6,24 +6,23 @@
 
 // @lc code=start
 function findTheDifference(s: string, t: string): string {
-  const hash = new Map()
+  const hash = new Map();
 
   for (let i = 0; i < s.length; i++) {
-    hash.set(s[i], (hash.get(s[i]) ?? 0) + 1)
+    hash.set(s[i], (hash.get(s[i]) ?? 0) + 1);
   }
 
   for (let i = 0; i < t.length; i++) {
     if (hash.has(t[i])) {
-      const value = hash.get(t[i])
+      const value = hash.get(t[i]);
       if (value > 1) {
-        hash.set(t[i], value - 1)
+        hash.set(t[i], value - 1);
       } else {
-        hash.delete(t[i])
+        hash.delete(t[i]);
       }
     } else {
-      return t[i]
+      return t[i];
     }
   }
-};
+}
 // @lc code=end
-
