5bbc9427284db2990237dfb50c0cd2226dd794de
Omar Camacho <116383442+camachoo1@users.noreply.github.com>
2024-04-19 16:36:56 -0700

    Add 21.merge-two-sorted-lists.go

---
+++ go/21.merge-two-sorted-lists.go
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+    res := &ListNode{0, nil}
+    i := res
+    for list1 != nil && list2 != nil {
+        if list1.Val <= list2.Val {
+            i.Next, list1 = list1, list1.Next
+        } else {
+            i.Next, list2 = list2, list2.Next
+        }
+        i = i.Next
+    }
+    if list1 != nil { i.Next = list1 } else { i.Next = list2 }
+    return res.Next
+}
